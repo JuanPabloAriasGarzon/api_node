eee19bd00fe0b3da03d5893f53996230ac9a0926
JuanPabloAriasGarzon <jupaga402@gmail.com>
2025-07-04 19:56:14 -0500

    CRUD java

--- index.js
+++ index.js
@@ -18,17 +18,86 @@ let usuarios=[
     {id:3, nombre:"jose", email:"jsoe@gmail.com"}
 ]
 
-app.get("/usuarios",(req,res)=>{
+app.get("/usuarios/todos",(req,res)=>{
     res.json(usuarios)
 });
 
 //obtener usuarios por id
-app.get("/usuarios/:id",(req,res)=>{
+app.get("/usuarios/buscar/:id",(req,res)=>{
     const id = parseInt(req.params.id);
     //console.log(id)
     const usuario=usuarios.find(user=>user.id===id)
+    //console.log(usuario)
+    if(!usuario){
+        res.status(404).json({
+            mensaje:"Usuario no encontrado"
+        })
+    }
     res.json(usuario)
 })
+
+
+//Crear un nuevo usuario CREATE
+app.post("/usuarios/crear",(req,res)=>{
+    //console.log(req.body)
+    const {nombre,email}=req.body;
+    //console.log(nombre,email)
+    const nuevoUsuario={
+        id: usuarios.length+1,
+        nombre:nombre,
+        email:email
+    }
+    //guardar el objeto creado en la BBDD
+    usuarios.push(nuevoUsuario)
+    res.status(201).json({
+        mensaje:"usuario creado correctamente",
+        usuarioCreado:nuevoUsuario
+    })
+})
+
+//Actualizar un usuario UPDATE
+app.put("/usuarios/actualizar/:id",(req,res)=>{
+    const {nombre,email}=req.body;
+    const id=parseInt(req.params.id);
+    //buscar
+    const usuario=usuarios.find(u=>u.id===id);
+    if(!usuario,!email){
+        res.status(404).json({
+            mensaje:"Usuario y correo a modificar no encontrado"
+        })
+    }
+    const infoAnterior=usuario.nombre;
+    usuario.nombre=nombre;
+
+    const emailanterior=usuario.email;
+    usuario.email=email;
+    res.status(202).json({
+        mensaje:"Usuario y correo modificados correctamente",
+        infoAnterior:infoAnterior,
+        infoNueva:usuario
+    })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
